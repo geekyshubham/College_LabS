4fd57fbf72884ed7e39bfcb3fc18f4dae7abd83f
Shubham Takankhar <takankhar@gmail.com>
2020-08-25 18:31:50 +0530

    ssup

--- CPP & Java OOPS/Assignments/function-overloading.cpp
+++ CPP & Java OOPS/Assignments/function-overloading.cpp
@@ -1,9 +1,9 @@
 Q1:write a program to calculate area triangle rectangle and circle using function overloading
 ==>
-#include<stdio.h>
+#include<iostream>
 #define pi 3.14
-int main()
-{
+using namespace std;
+
 float area(float r){
 	return(pi*r*r);
 }
@@ -12,24 +12,82 @@ float area(float b,float h){
 	return((b*h)/2);
 }
 
-float area(float w,float h){
+float area(float w,int h){
 	return(w*h);
 }
 
+
+int main()
+{
+float area(float r);
+float area(float w,int h);
+float area(float b,float h);
+
+
 int key;
-floar b,w,h = 0;
+float r,b,w,h = 0;
 do{
-	printf("Please Choose Appropriate Option");
-	print("1.area of circle\n2.area of triangle\n3.area of rectangle\nPRESS 0 TO EXIT");
-	scanf("%d",&key);
+	cout<<"Please Choose Appropriate Option";
+	cout<<"1.area of circle\n2.area of triangle\n3.area of rectangle\nPRESS 0 TO EXIT";
+  cin>>key;
 	switch(key){
-		case 1:printf("enter radius of circle");scanf("%f",&r);printf("area of circle is:%f",area(r));break;
-		case 2:printf("enter breadth and height of triangle");scanf("%f %f",&b,&h);printf("area of triangle is:%f",area(b,h));break;
-		case 3:printf("enter width and height of rectangle);scanf("%f %f",&w,&h);printf("area of rectangle is:%f",area(w,h));break;	
+		case 1:cout<<"\nenter radius of circle";
+    cin>>r;
+    cout<<"\narea of circle is: " << area(r);
+    break;
+		case 2:cout<<"\nenter breadth and height of triangle";cin>>b>>h;
+    cout<<"\narea of triangle is "<< area(b,h);
+    break;
+		case 3:cout<<"\nenter width and height of rectangle";
+    cin>>w>>h;
+    cout<<"\narea of rectangle is:%f",area(w,h);
+    break;
+    default:cout<<"\nenter Appropriate Option";
 	}
+}
 while(key!=0);
+
+
 return 0;
 }
+
+
+
+
+int main()
+{
+float area(float r);
+float area(float w,int h);
+float area(float b,float h);
+
+
+int key;
+float r,b,w,h = 0;
+do{
+	cout<<"Please Choose Appropriate Option";
+	cout<<"1.area of circle\n2.area of triangle\n3.area of rectangle\nPRESS 0 TO EXIT";
+  cin>>key;
+	switch(key){
+		case 1:cout<<"\nenter radius of circle";
+    cin>>r;
+    cout<<"\narea of circle is: " << area(r);
+    break;
+		case 2:cout<<"\nenter breadth and height of triangle";cin>>b>>h;
+    cout<<"\narea of triangle is "<< area(b,h);
+    break;
+		case 3:cout<<"\nenter width and height of rectangle";
+    cin>>w>>h;
+    cout<<"\narea of rectangle is:%f",area(w,h);
+    break;
+    default:cout<<"\nenter Appropriate Option";
+	}
+}
+while(key!=0);
+
+
+return 0;
+}
+
 
 
 Q2:write a program to find out absolute of integer variable double variable and float variable
@@ -52,5 +110,5 @@ double num(double num){
 	if (num<0){num=-num;return(num);}
 }
 
-}
+
 
