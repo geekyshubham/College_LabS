6019cec6b6d6cb8d566ceb8621a5ea1233f635c4
Shubham Takankhar <takankhar@gmail.com>
2020-09-11 12:20:25 +0530

    updated checkSum

--- CN/CHECKSUM/checksum.c
+++ CN/CHECKSUM/checksum.c
@@ -1,214 +1,118 @@
 
 char add(char x[], char y[], char sum[])
 
-
 {
-
 
     if (strlen(x) == strlen(y))
 
-
     {
-
 
         int length = strlen(x), i;
 
-
         char carry = '0';
 
-
-        for (i = length-1 ; i >= 0 ; i--)
-
+        for (i = length - 1; i >= 0; i--)
 
         {
 
-
-            
-
-
             if (x[i] == '1' && y[i] == '1' && carry == '0')
-
 
             {
 
-
                 carry = '1';
 
-
                 sum[i] = '0';
-
-
             }
-
 
             else if (x[i] == '1' && y[i] == '1' && carry == '1')
 
-
             {
-
 
                 carry = '1';
 
-
                 sum[i] = '1';
-
-
             }
-
 
             else if (x[i] == '1' && y[i] == '0' && carry == '0')
 
-
             {
-
 
                 carry = '0';
 
-
                 sum[i] = '1';
-
-
             }
-
 
             else if (x[i] == '1' && y[i] == '0' && carry == '1')
 
-
             {
-
 
                 carry = '1';
 
-
                 sum[i] = '0';
-
-
             }
-
 
             else if (x[i] == '0' && y[i] == '1' && carry == '0')
 
-
             {
-
 
                 carry = '0';
 
-
                 sum[i] = '1';
-
-
             }
-
 
             else if (x[i] == '0' && y[i] == '1' && carry == '1')
 
-
             {
-
 
                 carry = '1';
 
-
                 sum[i] = '0';
-
-
             }
-
 
             else if (x[i] == '0' && y[i] == '0' && carry == '0')
 
-
             {
-
 
                 carry = '0';
 
-
                 sum[i] = '0';
-
-
             }
-
 
             else
 
-
             {
-
 
                 carry = '0';
 
-
                 sum[i] = '1';
-
-
             }
-
-
         }
-
-
-        
-
-
     }
 
-
-    else
-
-
-    {
-
-
-        printf("\nWrong Input");
-
-
-    }
-
-
+    
 }
-
 
 
 char complement(char a[], char res[])
 
-
 {
-
 
     int length = strlen(a), i;
 
-
-    for (i = 0; i < length-1; i++)
-
+    for (i = 0; i < length - 1; i++)
 
     {
 
-
-
         if (a[i] == '0')
-
 
             res[i] = '1';
 
-
-        else if(a[i] == '0')
-
+        else if (a[i] == '0')
 
             res[i] = '0';
 
-
         else
 
-
-            res[i]=a[i];
-
-
+            res[i] = a[i];
     }
-
-
-    
-
-
 }
