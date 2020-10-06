72e9e6adbcdbe8932561466d426900f7b1df3d4a
Shubham Takankhar <takankhar@gmail.com>
2020-10-06 23:49:42 +0530

    validation on ele push

--- CPPJ/Assignments/stackUnderFlow.cpp
+++ CPPJ/Assignments/stackUnderFlow.cpp
@@ -1,81 +1,82 @@
-#include<iostream>
-#define size 5
+#include <iostream>
+#define size 4
 using namespace std;
 
-  class stack
-{
-     int data[size];
+class stack{
      int top;
-public:
-     stack()
-     {
+     int data[size];
+
+     public:
+     stack(){
           top=-1;
      }
-     void push();
-     void pop();
-     void display();
+     int push(int x){
+          try{
+          if (top>=size-1){
+               throw "STACK OVERFLOW";
+               
+          }
+          else{
+               data[++top]=x;
+          }} 
+          catch(const char* e){
+               cout<<"ERROR:"<<e<<endl;
+          }
+     }
+
+     int pop(){
+          try{
+          if (top>=0){
+               --top;
+               cout<<"Popped Element:"<<data[top+1]<<endl;
+          }
+          else{
+               throw "STACK UNDERFLOW";
+          }}
+          catch(const char* e){
+               cout<<"ERROR:"<<e<<endl;
+          }
+          
+     };
+     int display(){
+          int i = top;
+          cout<<"ELEMENTS ARE:";
+          while(i>=0){
+               cout<<data[i]<<" ";
+               --i;
+          }
+     }
+
 };
 
-void stack::push()
-{try{
-     if(top==size-1)
-     {
-          throw top;
-          return;
-     }
-     else
-     {
-          top++;
-          cout<<"Enter Data : ";
-          cin>>data[top];
-     }}
-     catch(...){
-          cout<<"Stack Overflow";
-     }
-}
-
-void stack::pop()
-{   try{
-     if(top==-1)
-          throw top;
-
-     else
-     {
-          cout<<data[top]<<"deleted "<<endl;
-          top--;
-     }
-}
-catch(int i){
-    cout<<"Stack Underflow";
-}
-
-}
-
-
-void stack::display()
-{
-     int t=top;
-     while(t>=0)
-     {
-          cout<<data[t]<<endl;
-          t--;
-     }
-}
-
-int main()
-{
-     stack st;
-     int ch;
-     do
-     {
-          cout<<"\n1. Push\n2. Pop\n3. Display \n4.Quit\nEnter Choice(1-4) ";
-          cin>>ch;
-          switch(ch)
-          {
-               case 1: st.push();break;
-               case 2: st.pop();break;
-               case 3: st.display();
+int main(){
+     stack s;
+     int x=0;
+     int temp;
+     cout<<"ENTER APPROPRIATE OPTION"<<endl;
+     while(x<4){
+          cout<<endl;
+          cout<<"1.PUSH"<<endl;
+          cout<<"2.POP"<<endl;
+          cout<<"3.DISPLAY"<<endl;
+          cout<<"EXIT WITH ANYTHING ELSE"<<endl;
+          cout<<"INPUT:";
+          cin>>x;
+          switch(x){
+               case 1:
+               cout<<"Enter Element to Insert:";
+               cin>>temp;
+               s.push(temp);
+               break;
+               case 2:
+               s.pop();
+               break;
+               case 3:
+               s.display();
+               break;
+               default:
+               return 1;
           }
-     }while(ch!=4);
+     }
 }
 
