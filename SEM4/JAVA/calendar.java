2d3cc7d00711667ff3831446a01c80cce4396dc7
Shubham Takankhar <takankhar@gmail.com>
2021-07-05 13:23:46 +0530

    fixed

--- SEM4/JAVA/calendar.java
+++
@@ -1,9 +0,0 @@
-import java.utils.*;
-import java.util.concurrent.LinkedBlockingQueue; 
-
-public class calendar{
-    public static void main(String[] argv){
-        StringBuilder sb = new StringBuilder("Good Morning");
-        sb.insert(1, "Friend ");
-        System.out.println(sb.toString()); 
-    }};
